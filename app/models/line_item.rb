b6241275e7e75a1ff8a3c3530f8a545523c3fa34
NestorW <nestor@smartlogicsolutions.com>
2012-06-13 16:33:15 -0400

    converted application view to haml, added totals to cart

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,4 +2,8 @@ class LineItem < ActiveRecord::Base
   attr_accessible :title, :body, :product, :quantity, :product_id, :cart_id
   belongs_to :product
   belongs_to :cart
+
+  def total_price
+  product.price * quantity
+  end
 end
